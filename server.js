42cec3208f11490c92c8edac175979e585e71aa0
george <george@billersmind.com>
2024-11-16 21:50:38 +0800

    Added port 0.0.0.0 on server.js

--- server.js
+++ server.js
@@ -55,8 +55,14 @@ app.get('/ping', (req, res) => {
 });
 
 
-// Start the server
+/*// Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
+});
+*/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('Server is running on http://0.0.0.0:3000');
 });
